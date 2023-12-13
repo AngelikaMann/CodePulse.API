9523ff61228ebf22218c11a7853c31b05a56e380
AngelikaMann <angelika0769@gmail.com>
2023-12-13 13:42:46 +0100

    Image Upload in ASP.NET Core and Angular.

---
+++ CodePulse.API/Controllers/ImagesController.cs
@@ -0,0 +1,67 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        //Post : {apibaseurl}/api/images
+        [HttpPost]
+
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file,
+            [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+            if (ModelState.IsValid)
+            {
+                //File upload
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    Title = title,
+                    FileName = fileName,
+                    DateCreated = DateTime.Now
+                };
+
+                blogImage = await imageRepository.Upload(file, blogImage);
+                //Convert Domain Model to DTO
+                var response = new BlogImageDto
+                {
+                    Id = blogImage.Id,
+                    Title = blogImage.Title,
+                    FileName = fileName,
+                    DateCreated = DateTime.Now,
+                    FileExtension = blogImage.FileExtension,
+                    Url = blogImage.Url
+                };
+                return Ok(response);
+            }
+            return BadRequest(ModelState);
+        }
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+            if (file.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size cannot be more than 10MB");
+            }
+        }
+
+
+    }
+}
