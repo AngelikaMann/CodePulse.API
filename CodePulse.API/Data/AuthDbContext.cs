487d366564c9de62ee83fa0204f653d0af8fc2eb
AngelikaMann <angelika0769@gmail.com>
2023-12-16 13:42:52 +0100

    Setting up Authentication(created user roles (reader and writer), created a default user, and also gave this default admin user the basic roles it needs). Configured the authentication core and configured authentication settings for the password added authentication, and added settings for JWT bearer parameters and token validation. Finally, authentication has been added to the HTTP pipeline.

---
+++ CodePulse.API/Data/AuthDbContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Create Reader and Writer Role
+
+            var readerRoleId = "c11e0541-8285-43d8-808e-9720d2ab07ab";
+            var writerRoleId = "536986b7-146c-4c62-907b-c4971cd29754";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id= readerRoleId,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper(),
+                    ConcurrencyStamp=readerRoleId
+                },
+                new IdentityRole()
+                {
+                 Id= writerRoleId,
+                    Name="Writer",
+                    NormalizedName="Writer".ToUpper(),
+                    ConcurrencyStamp=writerRoleId
+                }
+            };
+
+            //Seed the roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+
+
+            //Create an Admin User
+            var adminUserId = "e6c61a18-e9f0-43d0-83e4-832378064061";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin@codepulse.com",
+                Email = "admin@codepulse.com",
+                NormalizedUserName = "admin@codepulse.com".ToUpper(),
+                NormalizedEmail = "admin@codepulse.com".ToUpper()
+            };
+
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            //Give Roles To Admin
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId= adminUserId,
+                    RoleId=readerRoleId
+                },
+                new()
+                {
+                    UserId= adminUserId,
+                    RoleId=writerRoleId
+
+                }
+            };
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+        }
+    }
+}
--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -1,8 +1,11 @@
 using CodePulse.API.Data;
 using CodePulse.API.Repositories.Implementation;
 using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +17,56 @@ builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
 });
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
+
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepository>();
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("CodePulse")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+        };
+
+    });
+
+
 
 var app = builder.Build();
 
@@ -38,7 +84,7 @@ app.UseCors(options =>
     options.AllowAnyMethod();
     options.AllowAnyOrigin();
 });
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles(new StaticFileOptions
 {
